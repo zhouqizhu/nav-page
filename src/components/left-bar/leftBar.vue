468d7f9089508f0a90d9e6ae33260d2dab2666d5
zhouqizhu <luanww@111.com>
2021-09-10 19:49:55 +0800

    优化页面布局

--- src/components/left-bar/leftBar.vue
+++ src/components/left-bar/leftBar.vue
@@ -1,27 +1,24 @@
 <template>
-    <div id="left-bar">
-        <el-row>
-            <el-col :span="24">
-                <el-menu default-active="2" class="el-menu-vertical-demo" :collapse="$store.state.isCollapse == false">
-                    <!-- 图标站名 -->
-                    <div class="title">
-                        <img class="icon-logo" src="../../assets/explore.png" alt="图标">
-                        <span>XTab</span>  
-                    </div>
-                    <!-- 侧边栏导航 -->
-                    <el-sub-menu index="1" class="tab" v-for="(item,index) in navInfo" :key="index">
-                        <template #title class="tab-container" @click="toNAV(item.id)">
-                            <i :class="['fa', `fa-${item.icon}`,'tabIcon']"></i>
-                            <span>{{item.name}}</span>
-                        </template>
-                    </el-sub-menu>
-                    <!-- 添加侧边栏分类 -->
-                    <div class="tab add-tab">
-                        <tab-alert title="添加分类" elicon="el-icon-plus"></tab-alert>
-                    </div> 
-                </el-menu>
-            </el-col>
-        </el-row>    
+    <div>
+        <el-menu default-active="2" class="el-menu-vertical-demo" :collapse="$store.state.isCollapse == false">
+            <div id="left-bar">
+                <!-- 图标站名 -->
+            <div class="title">
+                <img class="icon-logo" src="../../assets/explore.png" alt="图标">
+                <span>XTab</span>  
+            </div>
+            <el-divider></el-divider>
+            <!-- 侧边栏导航 -->
+            <el-menu-item v-for="(item,index) in navInfo" :key="index" @click="toNAV(item.id)">
+                <i :class="['fa', `fa-${item.icon}`,'tabIcon']"></i>
+                <template #title>{{item.name}}</template>
+                </el-menu-item>
+                <!-- 添加侧边栏分类 -->
+                <div class="tab add-tab">
+                    <tab-alert title="添加分类" elicon="el-icon-plus"></tab-alert>
+                </div> 
+            </div>
+        </el-menu> 
     </div>
 </template>
 
@@ -30,12 +27,10 @@ import toNAV from '../../use/toNav'
 import { useStore } from 'vuex'
 import { computed } from '@vue/reactivity'
 import tabAlert from '../public/tabAlert.vue'
-// import {inject} from 'vue'
 export default {
     components: { tabAlert },
     setup() {
         const store = useStore()
-        // const $message = inject('message')      // 获取message组件方法
         return{
             toNAV,
             navInfo: computed(() => store.state.catalogue)
@@ -46,10 +41,9 @@ export default {
 
 <style lang="scss" scoped>
 #left-bar{
-    width: 250px;
     height: 100vh;
-    float: left;
-    // background-color: rgb(44, 42, 42);
+    position: fixed;
+    z-index: 99;
 }
 .el-sub-menu{
     cursor: pointer;
--- src/components/main-content/hcontent.vue
+++ src/components/main-content/hcontent.vue
@@ -1,16 +1,31 @@
 <template>
-    <!-- 控制侧边栏开合 -->
-    <el-radio-group @click="collapse" class="controlleBar">
-        <el-radio-button :label="false" v-if="$store.state.isCollapse ==false"><i class="el-icon-s-unfold"></i></el-radio-button>
-        <el-radio-button :label="true" v-else><i class="el-icon-s-fold"></i></el-radio-button>
-     </el-radio-group>
+    <div class="hcontent">
+        <!-- 控制侧边栏开合 -->
+        <span>
+            <el-radio-group @click="collapse" class="controlleBar">
+                <el-radio-button :label="false" v-if="$store.state.isCollapse ==false"><i class="el-icon-s-unfold"></i></el-radio-button>
+                <el-radio-button :label="true" v-else><i class="el-icon-s-fold"></i></el-radio-button>
+            </el-radio-group>
+        </span>
+        <div class="sentence">
+            <i class="el-icon-s-opportunity"></i>
+            <span id="hitokoto_text"></span>
+        </div>
+    </div>
 </template>
 
 <script>
 import {mapActions} from 'vuex'
 export default {
     setup() {
-        
+        fetch('https://v1.hitokoto.cn')
+            .then(response => response.json())
+            .then(data => {
+                const hitokoto = document.getElementById('hitokoto_text')
+                hitokoto.href = 'https://hitokoto.cn/?uuid=' + data.uuid
+                hitokoto.innerText = data.hitokoto
+            })
+            .catch(console.error)
     },
     methods: mapActions([
         'collapse'
@@ -19,4 +34,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.controlleBar{
+    top: 50%;
+    padding-right: 30px;
+}
+.sentence{
+    display: inline-block;
+    vertical-align: middle;
+    // margin-top: 20px;
+    width: 80%;
+    line-height: 50px;
+    height: 50px;
+    box-shadow: 0px 0px 3px #b48c8c;
+}
 </style>
